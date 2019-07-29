bb6e39eed7d0f06044cc9bef24dd318f7d682405
Joe Drago <jdrago@netflix.com>
2019-07-29 14:21:35 -0700

    nuke cmder, it is ancient

--- wbin/cmder/config/user-startup.cmd
+++
@@ -1,6 +0,0 @@
-:: use this file to run your own startup commands 
-:: use @ in front of the command to prevent printing the command
- 
-:: @call "C:\Users\jdrago\Downloads\cmder_mini\vendor\git-for-windows/cmd/start-ssh-agent.cmd
-:: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
- 
